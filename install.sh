961799f44b70b395d69df60d395510f99f6ba768
castor4bit <castor.4bit@gmail.com>
2014-06-30 08:48:20 +0900

    基準ディレクトリ指定誤り修正

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ FILES=(".gitconfig" ".gitignore" ".inputrc" ".screenrc" ".vimrc" ".zshrc")
 
 for file in ${FILES[@]}; do
   if [ ! -f "$HOME/$file" ]; then
-    ln -s `dirname $0`/$file $HOME/$file
+    ln -s $(cd $(dirname $0) && pwd)/$file $HOME/$file
   fi
 done
