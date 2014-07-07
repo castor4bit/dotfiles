73a7a46e8b099c97ecab7b00600848fa2f7283fc
castor4bit <castor.4bit@gmail.com>
2014-07-08 08:08:23 +0900

    Rename install.sh to setup.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-FILES=(".gitconfig" ".gitignore" ".inputrc" ".screenrc" ".vimrc" ".zshrc")
-
-for file in ${FILES[@]}; do
-  if [ ! -f "$HOME/$file" ]; then
-    ln -s $(cd $(dirname $0) && pwd)/$file $HOME/$file
-  fi
-done
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+FILES=(".gitconfig" ".gitignore" ".inputrc" ".screenrc" ".vimrc" ".zshrc")
+
+for file in ${FILES[@]}; do
+  if [ ! -f "$HOME/$file" ]; then
+    ln -s $(cd $(dirname $0) && pwd)/$file $HOME/$file
+  fi
+done
+
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
