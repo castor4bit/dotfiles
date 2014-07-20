1bada24fbdd8b05c58463b18fa25b22b10c54fee
castor4bit <castor.4bit@gmail.com>
2014-07-20 15:14:28 +0900

    Ruby/Perl/Node.jsインストールのコマンド群追加

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-FILES=(.gemrc .gitconfig .gitignore .inputrc .screenrc .vimrc .zshrc)
+# install dotfiles
+FILES=(.gemrc .gitconfig .gitignore .gvimrc .inputrc .screenrc .vimrc .zshrc)
 
 for file in ${FILES[@]}; do
   if [ ! -f "$HOME/$file" ]; then
@@ -8,4 +9,49 @@ for file in ${FILES[@]}; do
   fi
 done
 
+# change default shell to zsh
+grep '/usr/local/bin/zsh' /etc/shells > /dev/null
+if [ $? -ne 0 ]; then
+  sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+fi
+chsh -s /usr/local/bin/zsh
+
+# install ruby
+eval "$(rbenv init -)"
+CONFIGURE_OPTS="--enable-shared --with-readline-dir=/usr/local" rbenv install 2.1.2
+rbenv global 2.1.2
+rbenv rehash
+
+gem install bundler --no-ri --no-rdoc
+rbenv rehash
+
+# install perl
+curl -skL http://install.perlbrew.pl | bash
+source ~/perl5/perlbrew/etc/bashrc
+perl_version=$(perlbrew available | grep perl-5.20)
+perlbrew install $perl_version
+perlbrew switch  $perl_version
+
+perlbrew install-cpanm
+cpanm Data::Dumper LWP::UserAgent
+
+# install nodejs
+curl -sL git.io/nodebrew | perl - setup
+export PATH=$HOME/.nodebrew/current/bin:$PATH
+nodebrew selfupdate
+nodebrew install-binary v0.10.29
+nodebrew use v0.10.29
+
+npm install -g grunt-cli gulp
+npm install -g less
+npm install -g coffee-script
+npm install -g mocha expect.js testem sinon
+npm install -g hubot
+npm install -g casperjs
+
+# setup vim
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
+
+# enable locate
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
+sudo /usr/libexec/locate.updatedb
